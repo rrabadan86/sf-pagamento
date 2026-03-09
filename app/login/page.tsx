f6b238b6097ccd5bdb5709ad0e581e1b48dd833c
rrabadan86 <rodrigorabadan@gmail.com>
2026-03-09 13:54:34 -0300

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
-    const [email, setEmail] = useState("gestor@academia.com");
+    const [email, setEmail] = useState("slimfit.setorbueno@gmail.com");
     const [senha, setSenha] = useState("");
     const [erro, setErro] = useState("");
     const [loading, setLoading] = useState(false);
